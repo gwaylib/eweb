9313056be517eef194ac9cd41621939b2dcaa204
free1139 <free1139@163.com>
2023-07-16 08:53:12 +0000

    fix template

--- template.go
+++ template.go
@@ -15,25 +15,18 @@ type Template struct {
 func NewTemplate(tpl *template.Template) *Template {
 	return &Template{tpl}
 }
+
 func GlobTemplate(filePath string) *Template {
 	return &Template{template.Must(template.ParseGlob(filePath))}
 }
-func FSTemplate(fsys fs.FS, patterns ...string) (*Template, error) {
-	tpl, err := template.ParseFS(fsys, patterns...)
-	if err != nil {
-		return nil, err
-	}
-	return &Template{tpl}, nil
+func FSTemplate(fsys fs.FS, patterns ...string) *Template {
+	return &Template{template.Must(template.ParseFS(fsys, patterns...))}
 }
-func FilesTemplate(files ...string) (*Template, error) {
-	tpl, err := template.ParseFiles(files...)
-	if err != nil {
-		return nil, err
-	}
-	return &Template{tpl}, nil
+func FilesTemplate(files ...string) *Template {
+	return &Template{template.Must(template.ParseFiles(files...))}
 }
 
-// Implements Renderer interface
+// Implements echo.Renderer interface
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.ExecuteTemplate(w, name, data)
 }
