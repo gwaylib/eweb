b95c0730c4b2920b110fdd2e5b1254b900c40843
shu <free1139@163.com>
2023-10-31 13:46:41 +0800

    fix params

--- jsonp/params.go
+++ jsonp/params.go
@@ -18,10 +18,6 @@ var (
 
 type Params map[string]interface{}
 
-func NewParams() Params {
-	return make(Params)
-}
-
 func ParseParams(data []byte) (Params, error) {
 	params := Params{}
 	if err := json.Unmarshal(data, &params); err != nil {
